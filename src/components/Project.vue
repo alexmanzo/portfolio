a9b5fa0bb43a1de73ea48c32cbc61334fa5c05c2
alexmanzo <memanzo17@gmail.com>
2019-01-30 22:08:37 -0500

    created new project component, styling adjustments

---
+++ src/components/Project.vue
@@ -0,0 +1,111 @@
+<template>
+  <div
+    id="project"
+    class="project"
+    :class="hoverVisible"
+    @mouseover="hoverOn()"
+    @mouseleave="hoverOff()"
+  >
+    <h2 v-html="name">{{ name }}</h2>
+    <img :src="imgPath" :alt="name" :class="hoverVisible">
+    <p v-html="description" class="project--description">{{description}}</p>
+    <div class="project--links">
+      <a :href="codePath" target="_blank">
+        <i class="fab fa-github"></i>See the Code
+      </a>
+      <a :href="livePath" target="_blank">
+        <i class="fas fa-laptop-code"></i>See the Demo
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['name', 'imgPath', 'description', 'codePath', 'livePath'],
+  data() {
+    return {
+      hover: false,
+    }
+  },
+  methods: {
+    hoverOn() {
+      this.hover = true
+    },
+    hoverOff() {
+      this.hover = false
+    },
+  },
+  computed: {
+    hoverVisible() {
+      return {
+        hover: this.hover === true,
+      }
+    },
+  },
+}
+</script>
+
+
+<style lang="scss">
+@import 'main.scss';
+
+.hover {
+  /*background: rgba(0, 0, 0, 0.85);
+  color: white;*/
+  transition: opacity 1s;
+}
+
+img {
+  opacity: 1;
+  height: 100%;
+  margin: 0;
+  position: absolute;
+  transition: all 500ms ease;
+}
+
+img.hover {
+  opacity: 0;
+  z-index: -1;
+}
+
+.project {
+  width: 40%;
+  margin: 15px;
+  border: 5px solid $green;
+  position: relative;
+  overflow: hidden;
+  transition: all 1s;
+
+  h2 {
+    background-color: $green;
+    color: white;
+    text-align: center;
+    padding: 3px;
+    margin-block-start: 0;
+    margin-block-end: 0;
+  }
+
+  &--description {
+    padding: 0 40px;
+    text-align: justify;
+  }
+
+  &--links {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+
+    i {
+      font-size: 4vw;
+      padding: 10px;
+      color: $green;
+      display: block;
+    }
+    a {
+      color: $green;
+      padding: 20px;
+    }
+  }
+}
+</style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,25 +1,46 @@
 <template>
   <div id="projects">
-    <div v-for="(project, index) in projects" :key="index" class="project">
-      <img :src="project.screenshot" :alt="project.name" />
-      <h2 v-html="project.name">{{ project.name }}</h2>
-      <p v-html="project.description" class="project--description">{{project.description}}</p>
-      <div class="project--links">
-        <a href="`${codePath}`" target="_blank">
-          <i class="fab fa-github"></i>See the Code
-        </a>
-        <a href="`${livePath}`" target="_blank">
-          <i class="fas fa-laptop-code"></i>See the Demo
-        </a>
-      </div>
-    </div>
+    <project
+      :name="projects[0].name"
+      :imgPath="projects[0].screenshot"
+      :description="projects[0].description"
+      :codePath="projects[0].codePath"
+      :livePath="projects[0].livePath"
+    ></project>
+    <project
+      :name="projects[1].name"
+      :imgPath="projects[1].screenshot"
+      :description="projects[1].description"
+      :codePath="projects[1].codePath"
+      :livePath="projects[1].livePath"
+    ></project>
+    <project
+      :name="projects[2].name"
+      :imgPath="projects[2].screenshot"
+      :description="projects[2].description"
+      :codePath="projects[2].codePath"
+      :livePath="projects[2].livePath"
+    ></project>
+    <project
+      :name="projects[3].name"
+      :imgPath="projects[3].screenshot"
+      :description="projects[3].description"
+      :codePath="projects[3].codePath"
+      :livePath="projects[3].livePath"
+    ></project>
   </div>
 </template>
 
 <script>
+import project from '@/components/Project.vue'
+
 export default {
+  components: {
+    project,
+  },
   data() {
     return {
+      hover: false,
       projects: [
         {
           name: 'All-Gender Bathroom Finder',
@@ -27,7 +48,7 @@ export default {
             'This is a passion project that fulfills a need for myself and other non-binary and trans identifying individuals. <br><br> When on any given day you can be perceived as either male or female, something as simple as going to the bathroom can become a huge safety issue. This web app is a resource for finding all-gender bathrooms.',
           codePath: 'https://github.com/alexmanzo/bathroom-finder-app',
           livePath: 'https://bathroom-finder.alexmanzo.me/',
-          screenshot: '/src/assets/bathroom-finder.png',
+          screenshot: require(`../assets/bathroom-finder.png`),
         },
         {
           name: `Movie Search &ndash; Vue.js`,
@@ -35,14 +56,14 @@ export default {
             'Refactored project to learn Vue.js. Users can search for movies, find out basic details, then disocver more movies through exploring the cast, similar movies, and more.<br><br>One significant difference from the React version is the creation of a custom slider component.',
           codePath: 'https://github.com/alexmanzo/movie-search-vuejs',
           livePath: 'https://movie-search-vue.alexmanzo.me/',
-          screenshot: '/src/assets/movie-search-vue.png',
+          screenshot: require('../assets/movie-search-vue.png'),
         },
         {
           name: 'Movie Search &ndash; React',
           description: `This is a refactored version of a <a href="https://alexmanzo.github.io/movie-search-jquery/index.html" target=_blank>jQuery project</a>. Users can search for movies, find out basic details, then disocver more movies through exploring the cast, similar movies, and more.`,
           codePath: 'https://github.com/alexmanzo/movie-search-',
           livePath: 'https://movie-search-react.alexmanzo.me/',
-          screenshot: '/src/assets/movie-search-react.png',
+          screenshot: require('../assets/movie-search-react.png'),
         },
         {
           name: 'Crew Scheduler',
@@ -50,7 +71,7 @@ export default {
             'In my previous career, I managed a student group of 30-40 students to staff up to 100 live productions of sporting events throughout the year.<br><br>Managing availability and scheduling for several events with an array of needs, I wanted to develop an web application that would make this process easier.<br><br>Crew Scheduler allows as user to create all events for the year, and then have students log in to update their availability. When availability is in, the admin can go through an assign crew to events, and go back and edit as needed.',
           codePath: 'https://github.com/alexmanzo/crew-scheduler',
           livePath: 'https://fierce-wildwood-43879.herokuapp.com/',
-          screenshot: '/src/assets/crew-scheduler.png',
+          screenshot: require('../assets/crew-scheduler.png'),
         },
       ],
     }
@@ -69,41 +90,5 @@ export default {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-}
-
-.project {
-  width: 40%;
-  margin: 15px;
-  border: 5px solid $green;
-
-  h2 {
-    background-color: $green;
-    color: white;
-    text-align: center;
-    padding: 3px;
-    margin-block-start: 0;
-  }
-
-  &--description {
-    padding: 0 40px;
-    text-align: justify;
-  }
-
-  &--links {
-    display: flex;
-    justify-content: center;
-    text-align: center;
-
-    i {
-      font-size: 4vw;
-      padding: 10px;
-      color: $green;
-      display: block;
-    }
-    a {
-      color: green;
-      padding: 20px;
-    }
-  }
 }
 </style>
